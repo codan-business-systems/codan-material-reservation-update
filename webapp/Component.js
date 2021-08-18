43b7866ecf62e816a5061fc2e088156d2e940f13
acookoz <alex.cook@weavescape.com.au>
2021-08-18 00:34:52 +0000

    initial commit from wedide

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("upd.matres.codan.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
